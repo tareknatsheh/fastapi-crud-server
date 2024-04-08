4aa1ef06c76b0c1d455fe6d7d3027b3e2cf301d9
tareknatsheh <tarek.o.natsheh@gmail.com>
2024-04-08 13:15:51 +0300

    chat: send to all except the sender

--- routes/chat.py
+++ routes/chat.py
@@ -2,10 +2,16 @@ from fastapi import APIRouter, WebSocket
 
 router = APIRouter()
 
+other_chatters: list[WebSocket] = []
+
 @router.websocket("/chat")
 async def chat(websocket: WebSocket):
     await websocket.accept()
+    other_chatters.append(websocket)
 
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"You said: {data}")
+
+        for chatter in other_chatters:
+            if chatter != websocket:
+                await chatter.send_text(f"someone said {data}")
