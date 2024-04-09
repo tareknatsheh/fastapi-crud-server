f7a88fc2af65f6cc4b2c5a949207f805c06a3bde
tareknatsheh <tarek.o.natsheh@gmail.com>
2024-04-09 09:22:03 +0300

    separated chat and added more chat features

--- routes/chat.py
+++ routes/chat.py
@@ -1,17 +1,37 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from starlette.responses import FileResponse
 
 router = APIRouter()
-
 other_chatters: list[WebSocket] = []
 
-@router.websocket("/chat")
-async def chat(websocket: WebSocket):
+def profanity_check(msg: str):
+    msg = msg[:50]
+    return msg.capitalize()
+
+@router.get("/chat")
+def chat():
+    return FileResponse('public/chat.html')
+
+@router.websocket("/ws/{client_id}")
+async def chat_ws_endpoint(websocket: WebSocket, client_id: int):
+    """The school chat websocket endpoint, it handles the chat data traffic
+
+    Args:
+        websocket (WebSocket): represents the current user/chatter websocket, we can use it to interact only the user.
+    """
     await websocket.accept()
     other_chatters.append(websocket)
 
-    while True:
-        data = await websocket.receive_text()
-
+    try:
+        while True:
+            data = await websocket.receive_text()
+            data  = profanity_check(data)
+            await websocket.send_text(f"You: {data}")
+            for chatter in other_chatters:
+                if chatter != websocket:
+                    await chatter.send_text(f"{client_id}: {data}")
+    except WebSocketDisconnect:
+        other_chatters.remove(websocket)
         for chatter in other_chatters:
-            if chatter != websocket:
-                await chatter.send_text(f"someone said {data}")
+                if chatter != websocket:
+                    await chatter.send_text(f"User {client_id} has left")
